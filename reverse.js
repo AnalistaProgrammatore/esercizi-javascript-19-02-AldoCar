cd67538768b02edccd0569509e0db782e2866b0d
Aldo Caruso <aldocaruso1991@gmail.com>
2020-02-24 20:59:59 +0100

    prima parte

--- reverse.js
+++ reverse.js
@@ -2,3 +2,14 @@
 * Scrivere qui il codice dell'esercizio 2 del capitolo 4 di eloquentJS
 * Vi ricordo che le funzioni DEVONO ESSERE PURE e implementate una in modo IMPERATIVO e una in modo DICHIARATIVO
 **/
+
+let result = arrayRev()
+
+function arrayRev(){
+    let arrayValue = [1, 2, 3, 4, 5]
+    let arrayCopied = JSON.parse(JSON.stringify(arrayValue))
+    
+return arrayCopied.reverse()
+
+}
+console.log(result)
