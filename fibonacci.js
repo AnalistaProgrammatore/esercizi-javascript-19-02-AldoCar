1dd290cce9b90e92e23f0ab7e50f35360525c8c3
Aldo Caruso <aldocaruso1991@gmail.com>
2020-02-24 19:22:35 +0100

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,16 @@
 /**
 * Scrivere qui il codice della funzione fibonacci(n)
 **/
+function fibonacci(n) {
+    if (n <= 1) {
+      return n;
+    
+    } else {
+      return fibonacci(n - 1) + fibonacci(n - 2);
+      
+        }
+}
+
+for (let i = 0; i < 30; i++) {
+    console.log(fibonacci(i))
+}
