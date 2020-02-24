ff9e165682b4a123d9044c36d1937f93a3b92b54
Aldo Caruso <aldocaruso1991@gmail.com>
2020-02-24 19:23:48 +0100

    update

--- mcd.js
+++ mcd.js
@@ -1,3 +1,22 @@
 /**
 * Scrivere qui il codice delle funzioni mcd(x, y) e mcdRecursive(x, y)
 **/
+function mcd (x,y) {
+    while (y != 0 ) {
+        let r = x % y
+        x = y
+        y = r
+    }
+    return x
+}
+console.log(mcd(8, 10))
+
+
+// seconda parte
+
+
+function mcdRecursive(x, y) {
+    if (y === 0) return x
+    else return  mcd( y, (x % y))
+}
+console.log(mcd(68, 20))
